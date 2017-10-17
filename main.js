7200c9139cdd7fd137bc8a496b8791265eb19db9
designbright <ukmills12@gmail.com>
2017-10-17 13:31:40 -0400

    answered questions 7 - 10 on main.js

--- main.js
+++ main.js
@@ -42,42 +42,53 @@ math = high - "5";
 console.log(math);
 
 
-// // 7.
-// // Create a variable to calculate Tim's age
-// // The answer should read "Tim is 33 years old"
-// // The answer shoud not be written in a comment.
-// let born = 1984;
-// let today = 2017;
-// // A:
-//
-//
-// // 8.
-// // Adjust this code. Store some information in the following variables.
-// // A:
+// 7.
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 33 years old"
+// The answer shoud not be written in a comment.
+let born = 1984;
+let today = 2017;
+// A:
+let age = today - born;
+console.log(age);
+
+let timsAge = 'Tim is ' + age + ' years old.';
+console.log(timsAge);
+
+
+// 8.
+// Adjust this code. Store some information in the following variables.
+// A:
 // let yourName;
 // let instructorName;
-//
-//
-// // 9.
-// // Update the variables `yourName` and `instructorName` so the expression below reads correctly.
-// // A:
-//
-//
-// // This statement should read correctly
-// let statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
-//
-//
-// // 10.
-// // We've created a variable named after each primitive type.
-// // Use the `=` operator to assign a value to each variable matching the type in the name.
-// let myNumber;
-// let myString;
-// let myBoolean;
-// // This one's a little tricky - think carefully about what makes a value "undefined"!
-// let myUndefined;
-// // A:
-//
-//
+
+let yourName = "Kelly";
+let instructorName = "Kaitlin";
+
+// 9.
+// Update the variables `yourName` and `instructorName` so the expression below reads correctly.
+// A:
+
+// This statement should read correctly
+let statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+console.log(statement);
+
+// 10.
+// We've created a variable named after each primitive type.
+// Use the `=` operator to assign a value to each variable matching the type in the name.
+let myNumber = 3;
+let myString = 'This is a string';
+let myBoolean = false;
+// This one's a little tricky - think carefully about what makes a value "undefined"!
+let myUndefined;
+// A:
+
+console.log(typeof myNumber);
+console.log(typeof myString);
+console.log(typeof myBoolean);
+console.log(typeof myUndefined);
+
+
 // // For questions 11 - 19: List which boolean value will be stored in each variable. (Remember: `==` does not check data type)
 //
 // // 11.
